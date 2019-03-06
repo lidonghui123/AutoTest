baf767a5ad06f45dbacb0bafb80b40c79b649452
lidonghui123 <1323134804@qq.com>
2019-03-06 16:57:22 +0800

    test

--- Chapter6/src/main/java/com/tester/extend/demo/TestMethodDemo.java
+++ Chapter6/src/main/java/com/tester/extend/demo/TestMethodDemo.java
@@ -19,9 +19,14 @@ public class TestMethodDemo {
     public void test3(){
         Assert.assertEquals("aaa","aaa");
     }
-    @Test
-    public void logDemo(){
-        Reporter.log("这是我们自己写的日志");
-        throw  new RuntimeException("这是我们自己写的运行时候异常");
+     @Test
+    public void test4(){
+        Reporter.log("测试通过");
+        Assert.assertEquals(1,2);
     }
+   /* @Test
+    public void logDemo(){
+        Reporter.log("测试日志打印");
+        throw  new RuntimeException("这是我们自己写的运行时候异常");
+    }*/
 }
